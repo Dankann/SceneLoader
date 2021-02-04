4e1f2033785027502b2d8f155690a7e16b4544e6
Dankann <dankann.weissmuller@gmail.com>
2021-02-04 08:30:48 -0300

    Add unity project

---
+++ Packages/SceneLoader/Runtime/Extensions/TaskExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public static class TaskExtensions
+{
+    public static async void FireAndForgetAsync(this Task task)
+    {
+        try
+        {
+            await task;
+        }
+        catch
+        {
+            if (!task.IsCanceled)
+                throw;
+        }
+    }
+
+    public static async Task WaitUntilAsync(Func<bool> predicate, CancellationToken token = default)
+    {
+        try
+        {
+            while (!predicate() && !token.IsCancellationRequested)
+                await Task.Yield();
+        }
+        catch (NullReferenceException)
+        {
+            throw;
+        }
+    }
+
+    public static async Task WaitWhileAsync(Func<bool> predicate, CancellationToken token = default)
+    {
+        try
+        {
+            while (predicate() && !token.IsCancellationRequested)
+                await Task.Yield();
+        }
+        catch (NullReferenceException)
+        {
+            throw;
+        }
+    }
+
+    public static async Task DelayActionAsync(Action action, int milliseconds, CancellationToken token = default)
+    {
+        try
+        {
+            await Task.Delay(milliseconds, token);
+            action();
+        }
+        catch (NullReferenceException)
+        {
+            throw;
+        }
+    }
+
+    public static async Task DelayActionFramesAsync(Action action, int frames, CancellationToken token = default)
+    {
+        try
+        {
+            for (int i = 0; i < frames && !token.IsCancellationRequested; i++)
+                await Task.Yield();
+            action();
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
